3fdde9ff0aa73106d0b8f94d29d84dad4e15deb6
Bradley Beth <Bradley.beth@northernvermont.edu>
2020-10-13 13:41:42 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS3170_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             * PART I           
+             * 1. Ask for a sentence or phrase, and store as string variable named phrase.
+             * 2. Write the following to the console. Label each output:
+             *    a. The original string
+             *    b. The string in all lowercase characters
+             *    c. The string in all uppercase characters
+             *    d. All of the words in the string, each on its own line ending with a comma. 
+             *       (You can assume that no punctuation occurs in the phrase.)
+             *    e. Whether or not the phrase is a palindrome.s Do not consider spaces or 
+             *       upper- vs. lowercase when checking for palindromes.
+             *    f. The string sorted so that a < b < etc. This can be case sensitive, so:
+             *       "Northern Vermont University" --> "  NUVeeehiimnnnoorrrrstttvy"
+             *       
+             * PART II
+             * 3. Ask for a character, and store as an uppercase char named originalChar. If multiple
+             *    characters are entered, just use the first.
+             * 4. Ask for an offset (an integer between 0 and 25, inclusive.)
+             * 5. Rotate the character by the offset. So, if the original character is 'A' and the
+             *    offset is 2, the rotated character is 'C'. However, is the original character is
+             *    'Z', then the rotated character would be 'B'. Name this rotatedChar.
+             * 6. Write the following to the console. Label each output:
+             *    a. The original character, the offset, and the rotated character in the format:
+             *       "orignalChar rotated by offset is rotatedChar."
+             */
+
+            
+        }
+    }
+}
